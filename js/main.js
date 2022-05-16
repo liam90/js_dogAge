1a415519ffaa5eaf6baaa827d60a0ecdcc5fe0eb
Liam Mandeville <liamman1990@gmail.com>
2022-05-15 20:25:39 -0700

    getting the result to display

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 document.querySelector('#btn').addEventListener('click', dogYears);
 
 function dogYears() {
-    let firstTwoYears = 10.5;
     let userAge = document.querySelector('#inputAge').value;
-    userAge = userAge*4;
-    let dogAgeCalc = Math.floor(firstTwoYears + userAge);
-    let ageInDogYears = `This is your age in dog years ${dogAgeCalc}😲`;
-    document.h2.innerTEXT(ageInDogYears);
-    // console.log(ageInDogYears);
+    let firstTwoYearsAndAfter = Math.round(10.5 + userAge * 4);
+    ageInDogYears = `This is your age in dog years ${firstTwoYearsAndAfter}😲`;
+    document.querySelector('#displayDogAge').innerText = ageInDogYears;
 }
